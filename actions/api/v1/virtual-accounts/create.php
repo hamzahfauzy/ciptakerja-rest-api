f415b1ea7b91308c4b551777fa437c86e3c96a4b
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-01-10 11:48:26 +0700

    remove comment

--- actions/api/v1/virtual-accounts/create.php
+++ actions/api/v1/virtual-accounts/create.php
@@ -52,60 +52,6 @@ if(!empty($checker))
 }
 else
 {
-    
-    // $conn->query("INSERT INTO topup (userid,pesanan_nomor,pesanan_total,hp,va,status)VALUES('$userid','$pesanan_nomor','$pesanan_total','$hp','$va','MENUNGGU')");
-    // $id = $conn->insert_id;
-    // $data = $conn->query("SELECT * FROM topup WHERE id = $id");
-    // $data = $data->fetch_object();
-    // '{
-    //     "partnerServiceId":"", Derivative of X-PARTNER-ID , similar to company code, 8 digit left padding space. partnerServiceId + customerNo or virtualAccountNo
-    //     "customerNo":"", Unique number (up to 20 digits). partnerServiceId + customerNo or virtualAccountNo
-    //     "virtualAccountNo":"", partnerServiceId (8 digit left padding space) + customerNo (up to 20 digits). partnerServiceId + customerNo or virtualAccountNo
-    //     "virtualAccountName":"Jokul Doe", Customer name
-    //     "virtualAccountPhone":"6281828384858", Customer phone
-    //     "trxId":"abcdefgh1234", Transaction ID in Partner system
-    //     "totalAmount":{
-    //        "value":"12345678.00",
-    //        "currency":"IDR"
-    //     },
-    //     "billDetails":[
-    //        {
-    //           "billCode":"01",
-    //           "billNo":"123456789012345678",
-    //           "billName":"Bill A for Jan",
-    //           "billShortName":"Bill A",
-    //           "billDescription":{
-    //              "english":"Maintenance",
-    //              "indonesia":"Pemeliharaan"
-    //           },
-    //           "billSubCompany":"00001",
-    //           "billAmount":{
-    //              "value":"12345678.00",
-    //              "currency":"IDR"
-    //           },
-    //           "additionalInfo":{
-             
-    //           }
-    //        }
-    //     ],
-    //     "freeTexts":[
-    //        {
-    //           "english":"Free text",
-    //           "indonesia":"Tulisan bebas"
-    //        }
-    //     ],
-    //     "virtualAccountTrxType":"1",
-    //     "feeAmount":{
-    //        "value":"12345678.00",
-    //        "currency":"IDR"
-    //     },
-    //     "expiredDate":"2020-12-31T23:59:59-07:00",
-    //     "additionalInfo":{
-    //        "deviceId":"12345679237",
-    //        "channel":"mobilephone"
-    //     }
-    // }';
-    
     $va = generateVa();
     $body = '{
         "partnerServiceId":"'.config('snap_partner_service_id').'",
